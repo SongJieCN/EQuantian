cdf3cf914e9cc799ef6a72f9407c69c0fee712c8
song.jie <song.jie@cesgroup.com.cn>
2017-03-14 00:01:23 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='EQuantian',
+    version='1.0.0',
+    packages=['EQuant', 'EQuant.migrations', 'EQuantian'],
+    url='http://tigerquant.com',
+    license='MIT',
+    author='songjie',
+    author_email='songjie1984@gmail.com',
+    description=''
+)
